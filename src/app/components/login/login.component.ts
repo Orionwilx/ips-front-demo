25b6c0fb510a6deed60ec416068f11771f64b053
orionwilx <wilman3d@hotmail.com>
2024-11-01 21:17:32 -0500

    Frist commit, add tailwind, add login html and ts

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,59 @@
+import { HttpClient, HttpClientModule, HttpResponse } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { environment } from '../../../../enviroment';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule, HttpClientModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+
+export  class LoginComponent {
+
+  loginForm: FormGroup;
+  errorMessage: string = '';
+
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    console.log('Formulario enviado con los valores:', this.loginForm.value);
+    console.log('¿Formulario es válido?', this.loginForm.valid);
+
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
+      this.http.get(`${environment.apiUrl}/${email}`, { observe: 'response' })
+        .subscribe({
+          next: (response: HttpResponse<any>) => {
+            if (response && response.status === 200) {
+              console.log('Login successful, redireccionando...');
+              //localStorage.setItem('token', response.token);
+              this.router.navigate(['/home']);
+              console.log('Login successful');
+            }
+          },
+          error: (error: HttpResponse<any>) => {
+            if (error.status === 404) {
+              this.errorMessage = 'Usuario no encontrado. Por favor verifica tus credenciales.';
+              console.log('Usuario no encontrado. Por favor verifica tus credenciales.');
+            } else {
+              this.errorMessage = 'Error en el servidor. Inténtalo de nuevo más tarde.';
+            }
+          }
+        });
+    }
+  }
+}
